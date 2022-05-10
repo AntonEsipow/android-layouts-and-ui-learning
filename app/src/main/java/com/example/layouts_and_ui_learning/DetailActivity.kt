4f257f46ef4704dc2d8e26e2e9dbde8a9bfdd45e
AntonEsipow <esipow.anton99@gmail.com>
2022-05-10 16:07:05 +0300

    Passing data between activities

--- app/src/main/java/com/example/layouts_and_ui_learning/DetailActivity.kt
+++ app/src/main/java/com/example/layouts_and_ui_learning/DetailActivity.kt
@@ -1,6 +1,7 @@
 package com.example.layouts_and_ui_learning
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class DetailActivity : AppCompatActivity() {
@@ -8,5 +9,8 @@ class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        val textView: TextView = findViewById(R.id.textView)
+        textView.text = intent.getStringExtra("title") ?: "No title"
     }
 }
--- app/src/main/java/com/example/layouts_and_ui_learning/MainActivity.kt
+++ app/src/main/java/com/example/layouts_and_ui_learning/MainActivity.kt
@@ -4,15 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val titleTetView: TextView = findViewById(R.id.titleTextView)
+        val title = titleTetView.text
+
         val learnMoreButton: Button = findViewById(R.id.learnMoreButton)
         learnMoreButton.setOnClickListener {
             val intent = Intent(this@MainActivity, DetailActivity::class.java)
+            intent.putExtra("title", title)
             startActivity(intent)
         }
     }
